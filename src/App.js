4f72e6069cc817fa5f84b3717531d62e5d9cdf66
daahyunk <ekgusdl0408@naver.com>
2025-10-20 16:11:23 +0900

    feat(calculator): implement number conversion and sum logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import parseInput from './utils/parser.js';
-import convertToNumbers from './utils/calculator.js';
+import { convertToNumbers, sumNumbers } from './utils/calculator.js';
 import { ERROR_PREFIX } from './constants/error.js';
 
 class App {
@@ -9,7 +9,9 @@ class App {
       const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
       const parsed = parseInput(input);
       const numbers = convertToNumbers(parsed);
-      Console.print(`변환된 숫자 배열: [${numbers.join(', ')}]`);
+      const result = sumNumbers(numbers);
+
+      Console.print(`결과: ${result}`);
     } catch (error) {
       Console.print(`${ERROR_PREFIX} ${error.message}`);
     }
--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,21 +1,22 @@
 import { ERROR_MESSAGES } from '../constants/error.js';
 
-const convertToNumbers = (values) => {
-  // 값이 배열이 아닐 경우 에러
-  if (!Array.isArray(values)) {
-    throw new Error(ERROR_MESSAGES.INVALID_TYPE);
-  }
+export const convertToNumbers = (values) => {
+  if (!Array.isArray(values)) throw new Error(ERROR_MESSAGES.INVALID_TYPE);
 
   const numbers = values.map((value) => {
     const num = Number(value);
-
-    // 숫자가 아닌 경우
     if (Number.isNaN(num)) throw new Error(ERROR_MESSAGES.NOT_A_NUMBER);
-
     return num;
   });
 
   return numbers;
 };
 
-export default convertToNumbers;
+export const sumNumbers = (numbers) => {
+  if (!Array.isArray(numbers)) throw new Error(ERROR_MESSAGES.INVALID_TYPE);
+
+  const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+  if (Number.isNaN(sum)) throw new Error(ERROR_MESSAGES.INVALID_RESULT);
+
+  return sum;
+};
